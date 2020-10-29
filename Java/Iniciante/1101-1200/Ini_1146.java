66b5ecda572f9d833db4dd93b0f4d86571ebc0ff
Luiz Araujo <arauldesign@gmail.com>
2020-10-29 20:27:14 -0300

    1146 seq crescentes

---
+++ Java/Iniciante/1101-1200/Ini_1146.java
@@ -0,0 +1,43 @@
+package ini_1101_1200;
+
+/* 1146 - SEQUENCIAS CRESCENTES
+ * 
+ * PROBLEMA
+ * Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada 
+ * for igual a zero). Para cada valor lido imprima a sequência de 1 até X, com um espaço entre cada número e seu 
+ * sucessor.
+ * Obs: cuide para não deixar espaço em branco após o último valor apresentado na linha ou você receberá 
+ * Presentation Error.
+ * 
+ * ENTRADA
+ * O arquivo de entrada contém vários números inteiros. O último número no arquivo de entrada é 0.
+ * 
+ * SAÍDA
+ * Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo. 
+ * Não deve haver espaço em branco após o último valor da linha.
+ * 
+ * 
+ * 
+ * Author: Luiz Araujo
+ * */
+
+import java.util.Scanner;
+
+public class Ini_1146 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+        int x = input.nextInt();
+        
+        while (x != 0) {
+        	for (int i = 1; i <= x; i++) {
+        		if (i == x) System.out.print(i + "\n");
+        		else System.out.print(i + " ");
+        	}
+        	x = input.nextInt();
+        }
+        
+        input.close();
+	}
+}
